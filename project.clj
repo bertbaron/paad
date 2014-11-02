66fa90dd9d19bee981d8bc060faebc5a1eed553d
Bert Baron <bertusbaron@gmail.com>
2014-11-02 19:58:39 +0100

    Add cljsbuild to the project and restured the project to allow both clj and cljs sources

--- project.clj
+++ project.clj
@@ -3,7 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths ["src/clj" "src/cljs" "src/brepl"]
+
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [primitive-math "0.1.3" :scope "test"]]
-;  :aot :all
-  )
+                 [org.clojure/clojurescript "0.0-2069"]]
+
+  :plugins [[lein-cljsbuild "1.0.0"]]
+
+  ;; cljsbuild tasks configuration
+  :cljsbuild {:builds
+                [{;; clojurescript source code path
+                  :source-paths ["src/cljs"]
+
+                  ;; Google Closure Compiler options
+                  :compiler {;; the name of emitted JS script file
+                             :output-to "resources/public/js/paad.js"
+
+                             ;; minimum optimization
+                             :optimizations :whitespace
+
+                             ;; prettyfying emitted JS
+                             :pretty-print true}}]})  
